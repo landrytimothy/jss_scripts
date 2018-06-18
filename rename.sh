f5a43a6881d4343a8090214c2c6b3e373a8adf17
Timothy Landry <landrytimothy@gmail.com>
2018-06-18 09:16:33 -0500

    Added rename.sh

---
+++ rename.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#Pull the last 6 of the serial number
+serial=$(ioreg -rd1 -c IOPlatformExpertDevice | awk -F'"' '/IOPlatformSerialNumber/{print $4}' | tail -c 7)
+
+#Change computer names to Netspend naming convention + last 7 
+
+/usr/sbin/scutil --set ComputerName "OSX-$serial"
+/usr/sbin/scutil --set LocalHostName "OSX-$serial"
+/usr/sbin/scutil --set HostName "OSX-$serial"
+
+sudo jamf policy -trigger settime
+sudo jamf policy -trigger bindtoad
+
+#Flush dns cache
+dscacheutil -flushcache
+
+#Exit
+exit 0 
